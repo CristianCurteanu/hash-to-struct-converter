f1dfe11130aac6b13fed136fd010c3667dd85b7e
Cristian Curteanu <Cristian.Curteanu@4finance.com>
2018-03-15 15:53:04 +0200

    Fixed code style issues

--- hash_to_struct_mapper.rb
+++ hash_to_struct_mapper.rb
@@ -1,7 +1,7 @@
-require "pry-byebug"
-
 class Hash
   def to_struct(hash = self)
-    Struct.new(*hash.keys).new(*hash.values.map! { |v| (v.is_a?(Hash) ? to_struct(v) : v ) } )
+    Struct.new(*hash.keys).new(*hash.values.map! do |v|
+      v.is_a?(Hash) ? to_struct(v) : v
+    end)
   end
 end
